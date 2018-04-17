6481047daa7a0a5c02a752b30e96d279006f86c9
Orzobimbumbam <alberto.campi@aol.co.uk>
2018-04-17 22:04:44 +0200

    latest changes

---
+++ includes.h
@@ -0,0 +1,18 @@
+
+
+#ifndef Includes_h
+#define Includes_h
+
+//add all include directives to main here
+
+#include "schroddy.h"
+#include "parameters.h"
+#include "initpot.h"
+#include "eigenvalues.hpp"
+#include "densities.h"
+#include "kohn-sham.h"
+#include "filling.h"
+
+
+
+#endif /* Includes_h */
