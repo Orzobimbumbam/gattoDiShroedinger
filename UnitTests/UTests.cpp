2323ee32337e2dff18f34a094067eae23ade4c3d
Orzobimbumbam <alberto.campi@aol.co.uk>
2018-05-26 00:44:03 +0200

    Added TeamCity integration + unit test coverage

--- UnitTests/UTests.cpp
+++ UnitTests/UTests.cpp
@@ -10,6 +10,7 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/included/unit_test.hpp>
 #include "../Codice/Includes.h"
+#include "teamcity_messages.h"
 
 #include <fstream>
 #include <cmath>
---
+++ UnitTests/UnitTestsLaucher.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  UnitTestsLaucher.sh
+#  Codice
+#
+#  Created by Alberto Campi on 26/05/2018.
+#  Copyright © 2018 Alberto Campi. All rights reserved.
+
+DerivedData/Codice/Build/Products/Debug/UnitTests --result_code=no --report_level=no
