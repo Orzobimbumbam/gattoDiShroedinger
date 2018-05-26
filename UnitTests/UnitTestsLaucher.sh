2514983032bf01b69f0851250defba980f27a5fa
Orzobimbumbam <alberto.campi@aol.co.uk>
2018-05-26 02:32:01 +0200

    .

--- UnitTests/UnitTestsLaucher.sh
+++ UnitTests/UnitTestsLaucher.sh
@@ -6,4 +6,4 @@
 #  Created by Alberto Campi on 26/05/2018.
 #  Copyright © 2018 Alberto Campi. All rights reserved.
 
-DerivedData/Codice/Build/Products/Debug/UnitTests --result_code=no --report_level=no
+../DerivedData/Codice/Build/Products/Debug/UnitTests --result_code=no --report_level=no
